27d63488bceaec2e8969849dc87af2e1afc9d162
GBKS <chri@sto.ph>
2025-10-16 11:19:40 +0200

    Initial Commit

---
+++ Ark wallet prototype/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Ark wallet prototype
+//
+//  Created by Christoph on 10/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Ark wallet prototypeTests/Ark_wallet_prototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ark_wallet_prototypeTests.swift
+//  Ark wallet prototypeTests
+//
+//  Created by Christoph on 10/16/25.
+//
+
+import Testing
+@testable import Ark_wallet_prototype
+
+struct Ark_wallet_prototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
